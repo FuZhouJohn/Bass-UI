c79dfbda5af2353827b89e490f68f171ec2827b0
FuZhouJohn <a492073467@gmail.com>
2021-04-15 03:58:57 +0000

    Deploying to gh-pages from @ FuZhouJohn/Bass-UI@d7d6560e6014282c5d5a364e1a13395495fb03d3 🚀

---
+++ assets/en_index.md.c9dfed83.lean.js
@@ -0,0 +1,1 @@
+import{o as e,c as t,a as r}from"./app.09a43453.js";const a={components:{}},n='{"title":"","description":"","frontmatter":{},"relativePath":"en/index.md","lastUpdated":1618459131145}',o=r("p",null,"Write home page here.",-1);a.render=function(r,a,n,p,i,d){return e(),t("div",null,[o])};export default a;export{n as __pageData};
